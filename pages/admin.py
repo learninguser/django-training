40bea323c0d56f87b8ef202db0a3979d28923516
Pavan Kumar Akula <akulapavankumar@outlook.com>
2021-02-17 16:53:44 +0100

    customised admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
+
 # Register your models here.
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html(f'<img src = "{object.photo.url}" width = "40px" style = "border_radius: 100px" />')
+
+    thumbnail.short_description = 'photo'
+
+    list_display = ("id", "thumbnail", "first_name", "designation", "created_date")
+    list_display_links = ("id", "thumbnail", "first_name")
+    search_fields = ("first_name", "last_name", "designation")
+    list_filter = ("designation", )
+
+admin.site.register(Team, TeamAdmin)
