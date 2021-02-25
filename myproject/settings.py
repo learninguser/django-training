45f13fabb3e75a00b0c7e429b8ae7641ed39efec
Pavan Kumar Akula <akulapavankumar@outlook.com>
2021-02-25 15:45:44 +0100

    added Humanize feature to show for e.g. 1,000

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'cars.apps.CarsConfig',
     'ckeditor',
+    'django.contrib.humanize',
 
 ]
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Paris"
 
 USE_I18N = True
 
