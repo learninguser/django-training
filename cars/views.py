485b84ebb850b720cd0e659fc5421cebcb5c2051
Pavan Kumar Akula <akulapavankumar@outlook.com>
2021-02-28 10:58:30 +0100

    implemented cars and pagination

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('created_date')
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    page_cars = paginator.get_page(page)
+    context = {
+        'cars': page_cars,
+
+    }
+    return render(request, 'cars/cars.html', context=context)
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
