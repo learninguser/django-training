06c3c0dcea7be4e74bb7240a4f8aaeed5ebe8b49
Pavan Kumar Akula <akulapavankumar@outlook.com>
2021-03-03 08:50:13 +0100

    implemented search functionality - 1

--- cars/views.py
+++ cars/views.py
@@ -8,8 +8,18 @@ def cars(request):
     paginator = Paginator(cars, 4)
     page = request.GET.get('page')
     page_cars = paginator.get_page(page)
+
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    state_search = Car.objects.values_list('state', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     context = {
         'cars': page_cars,
+        'model_search': model_search,
+        'state_search': state_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
 
     }
     return render(request, 'cars/cars.html', context=context)
@@ -27,6 +37,33 @@ def search(request):
         keyword = request.GET['keyword']
         if keyword:
             cars = cars.filter(description__icontains=keyword)
+    
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+    
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            cars = cars.filter(state__iexact=state)
+    
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+
+        if 'max_price':
+            cars = cars.filter(price__gte = min_price, price__lte = max_price)
         
     context = {
         'cars': cars,
